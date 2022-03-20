8798a1d45fa5727145ad70297bb9371dcc3e4ead
Mujahedyousef <mujahedyousef97@gmail.com>
2022-03-21 01:14:02 +0300

    added new update

--- app/driver.js
+++ app/driver.js
@@ -1,51 +1,52 @@
 'use strict'
 
 const eventEmitter = require('../lib/events');
+
 const pickup = (payload) => {
-    let EVENT = {
-      event: "pickup",
-      time: new Date().toString(),
-      payload: payload
-    }
-    console.log(EVENT);
-    setTimeout(function() {
-      eventEmitter.emit('inTtransit', EVENT.payload);
-    }, 1000);
-    
-  
-  
+  let EVENT = {
+    event: "pickup",
+    time: new Date().toString(),
+    payload: payload
   }
-  eventEmitter.on('pickup', pickup);
-const inTtransit = (payload) => {
-    
-    
-    console.log(`DRIVER: picked up  ${payload.orderId}`);
-    let EVENT = {
-        event: 'in-transit',
-        time: new Date().toString(),
-        payload: payload
-    }
-    console.log(EVENT);
-    setTimeout(function() {
-        eventEmitter.emit('delivered', EVENT.payload);
-      }, 3000);
-    
+  console.log(EVENT);
+  setTimeout(() => {
+    eventEmitter.emit('inTtransit', EVENT.payload);
+  }, 1000);
 
 }
 
-eventEmitter.on('inTtransit', inTtransit);
+const inTtransit = (payload) => {
+
+
+  console.log(`DRIVER: picked up  ${payload.orderId}`);
+  let EVENT = {
+    event: 'in-transit',
+    time: new Date().toString(),
+    payload: payload
+  }
+  console.log(EVENT);
+  setTimeout(() => {
+    eventEmitter.emit('delivered', EVENT.payload);
+  }, 3000);
+
+
+}
+
+
 
 const delivered = (payload) => {
-    console.log(`DRIVER: delivered up  ${payload.orderId}`);
+  console.log(`DRIVER: delivered up  ${payload.orderId}`);
 
-    eventEmitter.emit('notification', payload);
-    let EVENT = {
-        event: 'delivered',
-        time: new Date().toString(),
-        payload: payload
-    }
-    console.log(EVENT);
+  eventEmitter.emit('notification', payload);
+  let EVENT = {
+    event: 'delivered',
+    time: new Date().toString(),
+    payload: payload
+  }
+  console.log(EVENT);
 
 
 }
+eventEmitter.on('pickup', pickup);
+eventEmitter.on('inTtransit', inTtransit);
 eventEmitter.on('delivered', delivered);
--- app/vendor.js
+++ app/vendor.js
@@ -1,29 +1,27 @@
 const eventEmitter = require('../lib/events');
 const { faker } = require('@faker-js/faker');
+
 require("./driver");
 
-
-const crateOrder = () => {
-let order ={
+const createOrder = () => {
+  let order = {
     store: '1-206-flowers',
     orderId: faker.random.uuid(),
     customer: `${faker.name.firstName()} ${faker.name.lastName()}`,
     address: `${faker.address.city()}, ${faker.address.stateAbbr()}`
   }
-    console.log("new order tobe picked up");
+  console.log("====New Order====");
   eventEmitter.emit('pickup', order);
-  
-  }
-  
-  eventEmitter.on('crateOrder', crateOrder);
+
+}
+
 
 
 const notification = (payload) => {
 
-  console.log(`vendor :Thank you for delivering ${payload.orderId}`);
-
+  console.log(`VENDOR :Thank you for delivering ${payload.orderId}`);
 
 }
-
+eventEmitter.on('createOrder', createOrder);
 eventEmitter.on('notification', notification);
 
